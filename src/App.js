235f2cfc91bed9e799c3b512ad2ae9fe3bb196cf
Alexander Heraimenka <a.heraimenka@gmail.com>
2020-05-20 10:39:06 -0700

    Install bootstrap

--- src/App.js
+++ src/App.js
@@ -1,6 +1,9 @@
 import React from 'react';
 import logo from './logo.svg';
+import 'bootstrap/dist/css/bootstrap.min.css';
 import './App.css';
+
+import RegisterForm from "./components/RegisterForm";
 
 function App() {
     return (
